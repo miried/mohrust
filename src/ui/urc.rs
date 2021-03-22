a6d83f3c440cc49c97af21db8b33063a276054d1
Michael Rieder <1219037+miried@users.noreply.github.com>
2021-03-22 13:59:23 +0100

    Added URC test

--- src/ui/urc.rs
+++ src/ui/urc.rs
@@ -3,21 +3,35 @@ use pest::Parser;
 #[grammar = "grammars/urc.pest"]
 pub struct URCParser;
 
-use crate::ui_println;
+//use crate::ui_println;
 
 pub fn parse_it( urc_string : &str )  {
     let file_parse = URCParser::parse(Rule::file, urc_string).expect("unsuccessful parse").next().unwrap();
     
+    println!("rec: {}", file_parse);
     for record in file_parse.into_inner() {
         match record.as_rule() {
             Rule::command => {
-                ui_println!("command {}", record);
+                println!("command {}", record);
             }
             Rule::resource => {
-                ui_println!("resource {}", record);
+                println!("resource {}", record);
+            }
+            Rule::name => {
+                println!("name {}", record.into_inner());
             }
             Rule::EOI => (),
-            _ => { ui_println!("TODO: {}", record);},
+            _ => { println!("TODO: {}", record);},
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    //#[should_panic]
+    fn internal() {
+        let testline = "menu \"main\" 639 479 NONE 0\nbgcolor 1 1 1 1\n";
+        assert_eq!((), super::parse_it(testline));
     }
 }
